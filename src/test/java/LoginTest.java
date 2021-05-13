4cd7f63c97e1083675a0086659d110ceb132c5d0
“EMILIA <EMILIAMAGHIAR@gmail.com>
2021-05-13 14:17:52 +0300

    Tema

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,16 +1,18 @@
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
 
+    @Test
     public void loginTestOne(){
 
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://testfasttrackit.info/selenium-test/");
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li.last > a")).click();
+        driver.findElement(By.cssSelector(".skip-account.skip-link > .label")).click();
+        driver.findElement(By.cssSelector("a[title='Log In']")).click();
         driver.findElement(By.cssSelector("#email")).sendKeys("emilia_ali@yahoo.com");
         driver.findElement(By.id("pass")).sendKeys("parola17");
         driver.findElement(By.cssSelector("#send2")).click();
--- src/test/java/registerTest.java
+++ src/test/java/registerTest.java
@@ -1,23 +1,26 @@
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class registerTest {
 
+    @Test
+
     public void registerTestOne (){
 
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://testfasttrackit.info/selenium-test/");
-        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
-        driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
+        driver.findElement(By.cssSelector(".skip-account.skip-link > .label")).click();
+        driver.findElement(By.cssSelector("a[title='Register']")).click();
         driver.findElement(By.cssSelector("#firstname")).sendKeys("Bogdan");
         driver.findElement(By.cssSelector("#middlename")).sendKeys("N");
         driver.findElement(By.cssSelector("#lastname")).sendKeys("BM");
         driver.findElement(By.cssSelector("#email_address")).sendKeys("bogdan.n.babalau@gmail.com");
         driver.findElement(By.cssSelector("#password")).sendKeys("parola17");
         driver.findElement(By.cssSelector("#confirmation")).sendKeys("parola17");
-        driver.findElement(By.cssSelector("#form-validate > div.fieldset > ul > li.control > label")).click();
+        driver.findElement(By.cssSelector(".control > label")).click();
 
         driver.quit();
 
